13b9f8c648c434265f3f4253311b11d37b4ac765
matej <matej.basta@email.cz>
2022-03-21 13:43:49 +0100

    Renaming folders

--- resources/js/storeit.js
+++ resources/js/storeit.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./storeit/App.jsx";
+import Storeit from "./storeit/Storeit.jsx";
 import "./storeit.css";
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(<Storeit />, document.querySelector("#app"));
