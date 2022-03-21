b995a1e3cd2b2cefbdcc8e171eaa4bb0f472372e
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-21 16:49:24 +0100

    change default role id for new users

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -36,7 +36,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
-            "role_id" => 1,
+            "role_id" => 4,
             "language_id" => 1
         ]);
     }
