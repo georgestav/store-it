b90aae8236c6dc40f3f434909ed192765c035b9f
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-22 21:32:17 +0100

    add booking search form by location

--- resources/js/storeit/content/search/SearchForm.jsx
+++ resources/js/storeit/content/search/SearchForm.jsx
@@ -1,139 +1,58 @@
-import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import CircularProgress from "@mui/material/CircularProgress";
-
-//Styles
+import React, { useState, useRef } from "react";
 import styles from "./SearchForm.module.css";
 
-function sleep(delay = 0) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, delay);
-    });
+function SearchForm({ setDisplaySearch }) {
+    const locationInput = useRef();
+
+    const sumbitSearchHandler = (e) => {
+        e.preventDefault();
+        window.location.href = "/results/" + locationInput.current.value;
+    };
+
+    return (
+        <form
+            className={styles.search__form__container}
+            onSubmit={sumbitSearchHandler}
+        >
+            <h3>Search your next Storage</h3>
+            <div>
+                <label htmlFor="location">Location</label>
+                <input
+                    id="location"
+                    type="text"
+                    placeholder="Location"
+                    ref={locationInput}
+                    autoFocus
+                />
+            </div>
+            <div>
+                <label htmlFor="fromDate">From date</label>
+                <input id="fromDate" type="date" disabled />
+            </div>
+            <div>
+                <label htmlFor="untilDate">Until Date</label>
+                <input id="untilDate" type="date" disabled />
+            </div>
+            <div>
+                <label htmlFor="location">Location</label>
+                <input
+                    id="location"
+                    type="text"
+                    placeholder="Location"
+                    disabled
+                />
+            </div>
+            <select name="" id="" disabled>
+                <option value="">Attic</option>
+                <option value="">Shed</option>
+                <option value="">Garage</option>
+                <option value="">Room</option>
+                <option value="">Locker</option>
+            </select>
+            <button onClick={() => setDisplaySearch(false)}>Back</button>
+            <button>Search</button>
+        </form>
+    );
 }
 
-export default function SearchForm() {
-    return <div className={styles.search__box}>test</div>;
-
-    // const [open, setOpen] = React.useState(false);
-    // const [options, setOptions] = React.useState([]);
-    // const loading = open && options.length === 0;
-
-    // React.useEffect(() => {
-    //     let active = true;
-
-    //     if (!loading) {
-    //         return undefined;
-    //     }
-
-    //     (async () => {
-    //         await sleep(1e3); // For demo purposes.
-
-    //         if (active) {
-    //             setOptions([...topFilms]);
-    //         }
-    //     })();
-
-    //     return () => {
-    //         active = false;
-    //     };
-    // }, [loading]);
-
-    // React.useEffect(() => {
-    //     if (!open) {
-    //         setOptions([]);
-    //     }
-    // }, [open]);
-
-    // return (
-    //     <Autocomplete
-    //         className={styles.locationSearch__container}
-    //         id="asynchronous-demo"
-    //         sx={{ width: 300 }}
-    //         open={open}
-    //         onOpen={() => {
-    //             setOpen(true);
-    //         }}
-    //         onClose={() => {
-    //             setOpen(false);
-    //         }}
-    //         isOptionEqualToValue={(option, value) =>
-    //             option.title === value.title
-    //         }
-    //         getOptionLabel={(option) => option.title}
-    //         options={options}
-    //         loading={loading}
-    //         renderInput={(params) => (
-    //             <TextField
-    //                 {...params}
-    //                 label="Asynchronous"
-    //                 InputProps={{
-    //                     ...params.InputProps,
-    //                     endAdornment: (
-    //                         <React.Fragment>
-    //                             {loading ? (
-    //                                 <CircularProgress
-    //                                     color="inherit"
-    //                                     size={20}
-    //                                 />
-    //                             ) : null}
-    //                             {params.InputProps.endAdornment}
-    //                         </React.Fragment>
-    //                     ),
-    //                 }}
-    //             />
-    //         )}
-    //     />
-    // );
-}
-
-// Top films as rated by IMDb users. http://www.imdb.com/chart/top
-const topFilms = [
-    { title: "The Shawshank Redemption", year: 1994 },
-    { title: "The Godfather", year: 1972 },
-    { title: "The Godfather: Part II", year: 1974 },
-    { title: "The Dark Knight", year: 2008 },
-    { title: "12 Angry Men", year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: "Pulp Fiction", year: 1994 },
-    {
-        title: "The Lord of the Rings: The Return of the King",
-        year: 2003,
-    },
-    { title: "The Good, the Bad and the Ugly", year: 1966 },
-    { title: "Fight Club", year: 1999 },
-    {
-        title: "The Lord of the Rings: The Fellowship of the Ring",
-        year: 2001,
-    },
-    {
-        title: "Star Wars: Episode V - The Empire Strikes Back",
-        year: 1980,
-    },
-    { title: "Forrest Gump", year: 1994 },
-    { title: "Inception", year: 2010 },
-    {
-        title: "The Lord of the Rings: The Two Towers",
-        year: 2002,
-    },
-    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { title: "Goodfellas", year: 1990 },
-    { title: "The Matrix", year: 1999 },
-    { title: "Seven Samurai", year: 1954 },
-    {
-        title: "Star Wars: Episode IV - A New Hope",
-        year: 1977,
-    },
-    { title: "City of God", year: 2002 },
-    { title: "Se7en", year: 1995 },
-    { title: "The Silence of the Lambs", year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: "Life Is Beautiful", year: 1997 },
-    { title: "The Usual Suspects", year: 1995 },
-    { title: "Léon: The Professional", year: 1994 },
-    { title: "Spirited Away", year: 2001 },
-    { title: "Saving Private Ryan", year: 1998 },
-    { title: "Once Upon a Time in the West", year: 1968 },
-    { title: "American History X", year: 1998 },
-    { title: "Interstellar", year: 2014 },
-];
+export default SearchForm;
