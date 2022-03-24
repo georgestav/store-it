beaa357b3b8ca86558b6f99618a403f6268d97a2
matej <matej.basta@email.cz>
2022-03-24 13:47:55 +0100

    almost done with bounding

--- resources/js/results/content/map/Map.jsx
+++ resources/js/results/content/map/Map.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import styles from "./map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 export default function Map({listings, cityCoordinates}) {
+
+    const all = listings.length ? listings : null;
+
+    let max = null;
+
+    if (all) {
+        all.forEach(element => {
+            const dist = Math.abs(element.coordinates.split(", ")[0] - cityCoordinates[0]);
+            if(dist > max) {
+                max = dist;
+            }
+        })
+
+        all.forEach(element => {
+            const dist = Math.abs(element.coordinates.split(", ")[1] - cityCoordinates[1]);
+            if (dist > max) {
+                max = dist;
+            }
+        })
+    }
+
+    console.log("max", max);
 
     return (
         <div>
             <h2>Map</h2>
             {cityCoordinates == false ?
             null :
-            (<MapContainer className={styles.container} center={cityCoordinates} zoom={10}>
+            (<MapContainer className={styles.container} center={cityCoordinates} bounds={max ? [[cityCoordinates[0] - max, cityCoordinates[1] - max], [[cityCoordinates[0] + max, [cityCoordinates[1] + max]]]] : null}>
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
