93743e379d4d9ce2aae0f8bd5cd68d5e9e7e3b34
matej <matej.basta@email.cz>
2022-03-24 11:05:28 +0100

    displaying distance

--- resources/js/results/content/listing/Listing.jsx
+++ resources/js/results/content/listing/Listing.jsx
@@ -1,14 +1,15 @@
 import styles from "./listing.module.css";
 import React, {useState} from "react";
 
-export default function Listing({listing, fetchAddress, address}) {
+export default function Listing({listing}) {
     
-    const {coordinates, daily_rate: dailyRate, description, rating, size, storage_type: storageType, user_id: userId} = listing;
+    const {distance, daily_rate: dailyRate, description, size} = listing;
 
         
     return (
         <div className={styles.container}>
-            <h3>Location: {address}</h3>
+            <h3>Location:</h3>
+            <p>Distance: {(distance / 1000).toFixed(2)} km</p>
             <p>Daily rate: {dailyRate}</p>
             <p>Description: {description}</p>
             <p>Size: {size}</p>
