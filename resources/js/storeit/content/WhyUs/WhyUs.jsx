f329a733baaa301bd65a42a09994e70e0d423463
Sean Matlock <beeredbeard@gmail.com>
2022-03-24 10:41:45 +0100

    landing page progress; blurred background, why us

--- resources/js/storeit/content/WhyUs/WhyUs.jsx
+++ resources/js/storeit/content/WhyUs/WhyUs.jsx
@@ -9,12 +9,32 @@ function WhyUs() {
 
                 <h1 className={styles.WhyUs}>Why us?</h1>
 
-                <div className={styles.container}>
+                <div className={styles.containers}>
 
-                    <h2 className={styles.efficient}>Efficient</h2>
-
-                    <p className={styles.paragraph}>Save hours by booking with our trusted clients</p>
+                    <div className={styles.container1}>
                     
+                        <h2 className={styles.headers}>Efficient</h2>
+
+                        <p className={styles.paragraph}>Save hours by booking with our trusted clients</p>
+
+                    </div>
+                    
+                    <div className={styles.container2}>
+                    
+                        <h2 className={styles.headers}>Secure</h2>
+
+                        <p className={styles.paragraph}>Know that your belongings are stored with verified, experienced hosts</p>
+
+                    </div>
+                    
+                    <div className={styles.container3}>
+                    
+                        <h2 className={styles.headers}>Accessible</h2>
+
+                        <p className={styles.paragraph}>Retrieve your property at your own convenience</p>
+
+                    </div>
+
                 </div>
 
             </div>
--- resources/js/storeit/content/ourprocess/OurProcess.jsx
+++ resources/js/storeit/content/ourprocess/OurProcess.jsx
@@ -12,42 +12,35 @@ function OurProcess() {
 
                 <div className={styles.body}>
 
-                    <div className={styles.ourProcessBackground}>
-                    {/* Uhh, dedicated div for background blur. 
-                    This is probably a bad practice but I don't 
-                    know what else to do.  */}
-                    </div>
-
-                    <h1 className={styles.ourProcess}>Our Process</h1>
-
-                    <div className={styles.containers}>
-
-                        <div className={styles.container1}>
-                            
-                            <h2>1</h2>
-                            <p>Book an available listing through our website</p>
-                            <img src={customers1} />
-
+                        <h1 className={styles.ourProcess}>Our Process</h1>
+    
+                        <div className={styles.containers}>
+    
+                            <div className={styles.container1}>
+                                
+                                <h2>1</h2>
+                                <p>Book an available listing through our website</p>
+                                <img src={customers1} />
+    
+                            </div>
+    
+                            <div className={styles.container2}>
+    
+                                <h2>2</h2>
+                                <p>Store your belongings</p>
+                                <img src={customers2} />
+    
+                            </div>
+    
+                            <div className={styles.container3}>
+    
+                                <h2>3</h2>
+                                <p>Retrieve at the end of booking!</p>
+                                <img src={customers3} />
+    
+                            </div>
+    
                         </div>
-
-                        <div className={styles.container2}>
-
-                            <h2>2</h2>
-                            <p>Store your belongings</p>
-                            <img src={customers2} />
-
-                        </div>
-
-                        <div className={styles.container3}>
-
-                            <h2>3</h2>
-                            <p>Retrieve at the end of booking!</p>
-                            <img src={customers3} />
-
-                        </div>
-
-                    </div>
-
 
                 </div>
 
