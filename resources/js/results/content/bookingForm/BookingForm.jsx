431a262ac031ebd8fd11982e0b930066711ddedb
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-29 14:17:01 +0200

    display available dates on booking form and show options on success

--- resources/js/results/content/bookingForm/BookingForm.jsx
+++ resources/js/results/content/bookingForm/BookingForm.jsx
@@ -1,23 +1,20 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../../../components/context/UserContext";
 import axios from "axios";
+import AvailableDates from "./AvailableDates";
+import { Button } from "@mui/material";
 
 export default function BookingForm() {
-
-    const {listingId} = useParams();
-
+    const { listingId } = useParams();
     const user = useContext(UserContext);
 
     const [availability, setAvailability] = useState(true);
     const [refreshTrigger, setRefreshTrigger] = useState(false);
     const [invalidDates, setInvalidDates] = useState(false);
+    const [bookingSubmited, setBookingSubmited] = useState(false); //default false
 
     const userId = user.user.id;
-    
-    // console.log(userId);
-
-    // console.log("haha");
 
     const [values, setValues] = useState({
         user_id: userId,
@@ -25,26 +22,23 @@ export default function BookingForm() {
         status: "pending",
         booked_from: "",
         booked_until: "",
-    })
+    });
 
     //function to trigger a refresh
     const forceRefresh = () => {
         setRefreshTrigger(!refreshTrigger);
     };
 
-    // console.log(values);
-    // console.log(userId);
-
     const handleChange = (event) => {
-        setValues(oldValues => {
+        setValues((oldValues) => {
             return {
                 ...oldValues,
-                [event.target.name]: event.target.value
-            }
+                [event.target.name]: event.target.value,
+            };
         });
         setAvailability(true);
         setInvalidDates(false);
-    }
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -59,26 +53,79 @@ export default function BookingForm() {
         if (data === "false order") {
             setInvalidDates(true);
         }
+        setBookingSubmited(true); //if booking is successful set to true to display a status message
 
         forceRefresh();
-    }
-
+    };
 
     return (
         <>
-        {availability ? <></> : <p><strong>Not available at this date</strong></p>}
-        {invalidDates ? <p><strong>Invalid order of dates.</strong></p> : <></>}
-        <form action="" method="post" onSubmit={handleSubmit}>
-            <label htmlFor="book-from">Book from</label>
-            <br />
-            <input id="book-from" name="booked_from" type="date" value={values.booked_from} onChange={handleChange} />
-            <br />
-            <label htmlFor="book-until">Book until</label>
-            <br />
-            <input type="date" name="booked_until" id="book-until" value={values.booked_until} onChange={handleChange}  />
-            <br />
-            <button>Book</button>
-        </form>
+            <AvailableDates listingId={listingId} />
+            {availability ? (
+                <></>
+            ) : (
+                <p>
+                    <strong>Not available at this date</strong>
+                </p>
+            )}
+            {invalidDates ? (
+                <p>
+                    <strong>Invalid order of dates.</strong>
+                </p>
+            ) : (
+                <></>
+            )}
+            {bookingSubmited === true ? (
+                <div>
+                    <div>Your booking was Successful</div>
+                    <div>Thank you!</div>
+                    <div>
+                        Now that your booking is pending you can go back to the
+                        home page or visit your profile to view your booking
+                        status
+                    </div>
+                    <div>
+                        <Button
+                            onClick={() => {
+                                window.location.href = "/";
+                            }}
+                        >
+                            Home
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                window.location.href = "/hosting";
+                            }}
+                        >
+                            Profile
+                        </Button>
+                    </div>
+                </div>
+            ) : (
+                <form action="" method="post" onSubmit={handleSubmit}>
+                    <label htmlFor="book-from">Book from</label>
+                    <br />
+                    <input
+                        id="book-from"
+                        name="booked_from"
+                        type="date"
+                        value={values.booked_from}
+                        onChange={handleChange}
+                    />
+                    <br />
+                    <label htmlFor="book-until">Book until</label>
+                    <br />
+                    <input
+                        type="date"
+                        name="booked_until"
+                        id="book-until"
+                        value={values.booked_until}
+                        onChange={handleChange}
+                    />
+                    <br />
+                    <button>Book</button>
+                </form>
+            )}
         </>
     );
 }
--- resources/js/results/content/detailedInfo/DetailedInfo.jsx
+++ resources/js/results/content/detailedInfo/DetailedInfo.jsx
@@ -1,13 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {useParams, Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import styles from "./detailedInfo.module.css";
 import Rating from "@mui/material/Rating";
 
 export default function DetailedInfo() {
+    const { id } = useParams();
 
-    const {id} = useParams();
-    
     // console.log(`api/listings/${id}`);
 
     // console.log(id);
@@ -16,45 +15,61 @@ export default function DetailedInfo() {
 
     //function that fetches the listing
     const fetchListing = async (id) => {
-
         try {
             const response = await axios.get(`api/listings/${id}`);
             const data = response.data;
             setListing(data);
             // console.log(data);
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
-    }
+    };
 
     useEffect(() => {
         fetchListing(id);
-    }, [])
+    }, []);
 
-    
     return (
         <>
-            {listing ?
-            <>
-            <img className={styles.image} src={listing.pictures.length > 0 ? `data:image/jpeg;base64,${listing.picutres[0].photo}` : ""} alt="" />
-            <Rating
-                                name="read-only"
-                                value={listing.rating}
-                                readOnly
-                            /> 
-            <h3>Location: {listing.address}</h3>
-            {listing.user.person != null ? <p>Name: {listing.user.person.name} {listing.user.person.surname}</p>  : <p>By user: {listing.user.name}</p> }
-            <p>Email: {listing.user.email}</p>
-            {listing.user.person != null && listing.user.person != null ? <p>Phone: {listing.user.person.phone}</p> : <></>}
-            <p>City: {listing.city.name}</p>
-            <p>Country: {listing.country.name}</p>
-            <p>Daily rate: {listing.daily_rate}</p>
-            <p>Description: {listing.description}</p>
-            <p>Rating: {listing.rating}</p>
-            <p>Size: {listing.size}m2</p>
-            <Link to={`/results/book/${listing.id}`}><button>Book</button> </Link>
-            </>
-            : null}
+            {listing ? (
+                <>
+                    <img
+                        className={styles.image}
+                        src={
+                            listing.pictures.length > 0
+                                ? `data:image/jpeg;base64,${listing.pictures[0].photo}`
+                                : ""
+                        }
+                        alt=""
+                    />
+                    <Rating name="read-only" value={listing.rating} readOnly />
+                    <h3>Location: {listing.address}</h3>
+                    {listing.user.person != null ? (
+                        <p>
+                            Name: {listing.user.person.name}{" "}
+                            {listing.user.person.surname}
+                        </p>
+                    ) : (
+                        <p>By user: {listing.user.name}</p>
+                    )}
+                    <p>Email: {listing.user.email}</p>
+                    {listing.user.person != null &&
+                    listing.user.person != null ? (
+                        <p>Phone: {listing.user.person.phone}</p>
+                    ) : (
+                        <></>
+                    )}
+                    <p>City: {listing.city.name}</p>
+                    <p>Country: {listing.country.name}</p>
+                    <p>Daily rate: {listing.daily_rate}</p>
+                    <p>Description: {listing.description}</p>
+                    <p>Rating: {listing.rating}</p>
+                    <p>Size: {listing.size}m2</p>
+                    <Link to={`/results/book/${listing.id}`}>
+                        <button>Book</button>{" "}
+                    </Link>
+                </>
+            ) : null}
         </>
     );
 }
