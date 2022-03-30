d3d4343441f7a1f7a80955cc9cde542bcc554213
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-30 11:50:51 +0200

    production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,8 +9,8 @@ window._ = require("lodash");
 window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.baseURL = "http://localhost:3000"; //changed to local host to work in development
-// "http://www.storeit.com/", //switch to this for production
+window.axios.defaults.baseURL = "storeit.codeboot.cz"; //changed to local host to work in development
+// "storeit.codeboot.cz", //switch to this for production
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
