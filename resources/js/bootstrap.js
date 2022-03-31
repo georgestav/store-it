5f947e8b8a0abb95eb7aeb15da534253e946c314
matej <matej.basta@email.cz>
2022-03-31 14:19:19 +0200

    commented-out bootstrap baseURL setting

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,8 +9,7 @@ window._ = require("lodash");
 window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.baseURL = "http://storeit.codeboot.cz"; //changed to local host to work in development
-//switch to this for production development -  http://localhost:3000;
+// window.axios.defaults.baseURL = ""; //changed to local host to work in development
 //switch to this for production - https://storeit.codeboot.cz/
 
 /**
