04d87897f57f50cbdae45a9c61a22d11e90423b2
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-31 11:14:45 +0200

    change bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,7 @@ window._ = require("lodash");
 window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.baseURL = "https://storeit.codeboot.cz/"; //changed to local host to work in development
+window.axios.defaults.baseURL = "https://storeit.codeboot.cz"; //changed to local host to work in development
 //switch to this for production development -  http://localhost:3000;
 //switch to this for production - https://storeit.codeboot.cz/
 
